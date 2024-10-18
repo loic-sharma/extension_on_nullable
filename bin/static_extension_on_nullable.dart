d9047dd6c42e78f70075b172b6f2950c215e2001
Loic Sharma <sharma.loic@gmail.com>
2024-10-18 15:11:39 -0700

    Start

---
+++ bin/static_extension_on_nullable.dart
@@ -0,0 +1,5 @@
+import 'package:static_extension_on_nullable/static_extension_on_nullable.dart' as static_extension_on_nullable;
+
+void main(List<String> arguments) {
+  print('Hello world: ${static_extension_on_nullable.calculate()}!');
+}
---
+++ test/static_extension_on_nullable_test.dart
@@ -0,0 +1,8 @@
+import 'package:static_extension_on_nullable/static_extension_on_nullable.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
